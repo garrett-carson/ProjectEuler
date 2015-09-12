3b62d655cba197f5fc7d263f5159f6f96dec01fb
Garrett Carson <Garrett.Carson@bdcsoft.com>
2015-09-11 22:57:50 -0500

    Problem 4

---
+++ ProjectEuler.Lib/Problem4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Numerics;
+
+namespace ProjectEuler.Lib {
+
+    // A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+    // 
+    // Find the largest palindrome made from the product of two 3-digit numbers.
+    [Export(typeof(Problem))]
+    public class Problem4 : Problem {
+        public override string Run() {
+            return LargestPalinedromeFromTwoNumbers(3).ToString();
+        }
+
+        public int LargestPalinedromeFromTwoNumbers(int digits) {
+            return Enumerable.Range(1, (int)Math.Pow(10, digits)).ToList().SelectMany(x => {
+                return Enumerable.Range(1, x).Select(y => x*y).Where(IsPalindrome);
+            }).OrderByDescending(x => x).First();
+        }
+
+        private bool IsPalindrome(int arg) {
+            var charArray = arg.ToString().ToCharArray();
+            Array.Reverse(charArray);
+            return arg.ToString() == new String(charArray);
+        }
+    }
+}
---
+++ ProjectEuler.Test/Problem4Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler.Lib;
+
+namespace ProjectEuler.Test {
+    [TestClass]
+    public class Problem4Test {
+        [TestMethod]
+        public void Problem4Example() {
+            // Arrange
+            var problem = new Problem4();
+
+            // Act
+            var result = problem.LargestPalinedromeFromTwoNumbers(2);
+
+            // Assert
+            Assert.AreEqual(9009, result);
+        }
+
+        [TestMethod]
+        public void Problem4Answer() {
+            // Arrange
+            var problem = new Problem4();
+
+            // Act
+            var result = problem.Answer;
+
+            // Assert
+            Assert.AreEqual("906609", result);
+        }
+    }
+}
