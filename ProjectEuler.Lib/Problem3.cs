0d046aed1c511629bb989f6612873aeeca6d3a61
Garrett Carson <Garrett.Carson@bdcsoft.com>
2015-09-11 22:34:38 -0500

    Problem 3

---
+++ ProjectEuler.Lib/Problem3.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Numerics;
+
+namespace ProjectEuler.Lib {
+
+    // The prime factors of 13195 are 5, 7, 13 and 29.
+    // 
+    // What is the largest prime factor of the number 600851475143 ?
+    [Export(typeof(Problem))]
+    public class Problem3 : Problem {
+        public override string Run() {
+            return LargestPrimeFactor(600851475143).ToString();
+        }
+
+        public ulong LargestPrimeFactor(ulong number) {
+            return PrimeFactors(number).OrderByDescending(x => x).First();
+        }
+
+        private static IEnumerable<ulong> PrimeFactors(ulong number) {
+            ulong a, b;
+            a = number;
+            for (b = 2; a > 1; b++) {
+                if (a % b == 0) {
+                    while (a % b == 0) {
+                        a /= b;
+                    }
+                    yield return b;
+                }
+            }
+        }
+    }
+}
---
+++ ProjectEuler.Test/Problem3Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler.Lib;
+
+namespace ProjectEuler.Test {
+    [TestClass]
+    public class Problem3Test {
+        [TestMethod]
+        public void Problem3Example() {
+            // Arrange
+            var problem = new Problem3();
+
+            // Act
+            var result = problem.LargestPrimeFactor(13195);
+
+            // Assert
+            Assert.AreEqual((ulong)29, result);
+        }
+
+        [TestMethod]
+        public void Problem3Answer() {
+            // Arrange
+            var problem = new Problem3();
+
+            // Act
+            var result = problem.Answer;
+
+            // Assert
+            Assert.AreEqual("6857", result);
+        }
+    }
+}
