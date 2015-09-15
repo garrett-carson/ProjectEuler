71502f44ec5b5e99e676d0f813b7231603608283
Garrett Carson <Garrett.Carson@bdcsoft.com>
2015-09-15 14:48:18 -0500

    Problem 7

---
+++ ProjectEuler.Lib/Problem7.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+namespace ProjectEuler.Lib {
+
+    // By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+    // 
+    // What is the 10 001st prime number?
+    [Export(typeof(Problem))]
+    public class Problem7 : Problem {
+        public override string Run() {
+            return NthUlongPrime(10001).ToString();
+        }
+
+        public ulong NthUlongPrime(int index) {
+            return UlongPrimes().ElementAt(index - 1);
+        }
+
+        private static IEnumerable<ulong> UlongPrimes() {
+            var memoized = new List<ulong>();
+            var primes = PotentialUlongPrimes()
+                .Where(x => {
+                    double sqrt = Math.Sqrt(x);
+                    return memoized
+                        .TakeWhile(y => y <= sqrt)
+                        .All(y => x % y != 0);
+                });
+            foreach (var prime in primes) {
+                yield return prime;
+                memoized.Add(prime);
+            }
+        }
+
+        private static IEnumerable<ulong> PotentialUlongPrimes() {
+            yield return 2;
+            yield return 3;
+            ulong k = 1;
+            while (k > 0) {
+                yield return 6 * k - 1;
+                yield return 6 * k + 1;
+                k++;
+            }
+        }
+    }
+}
---
+++ ProjectEuler.Test/Problem7Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler.Lib;
+
+namespace ProjectEuler.Test {
+    [TestClass]
+    public class Problem7Test {
+        [TestMethod]
+        public void Problem7Example() {
+            // Arrange
+            var problem = new Problem7();
+
+            // Act
+            var result = problem.NthUlongPrime(6);
+
+            // Assert
+            Assert.AreEqual((ulong)13, result);
+        }
+
+        [TestMethod]
+        public void Problem7Answer() {
+            // Arrange
+            var problem = new Problem7();
+
+            // Act
+            var result = problem.Answer;
+
+            // Assert
+            Assert.AreEqual("104743", result);
+        }
+    }
+}
