bd60c3dccee972a6352f75d3cf90695f8e8aba42
Garrett Carson <garrett.carson@bdcsoft.com>
2015-09-18 16:57:57 -0500

    Problem 11

---
+++ ProjectEuler.Test/Problem11Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler.Lib;
+
+namespace ProjectEuler.Test {
+    [TestClass]
+    public class Problem11Test {
+        [TestMethod]
+        public void Problem11Answer() {
+            // Arrange
+            var problem = new Problem11(new Problem11Input());
+
+            // Act
+            var result = problem.Answer;
+
+            // Assert
+            Assert.AreEqual("70600674", result);
+        }
+    }
+}
