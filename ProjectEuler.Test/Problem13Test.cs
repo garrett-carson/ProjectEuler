1d6242401416765b4d3d7766f09c7524cb048bf9
Garrett Carson <Garrett.Carson@bdcsoft.com>
2015-09-19 20:53:03 -0500

    Problem 13

---
+++ ProjectEuler.Test/Problem13Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler.Lib;
+
+namespace ProjectEuler.Test {
+    [TestClass]
+    public class Problem13Test {
+        [TestMethod]
+        public void Problem13Answer() {
+            // Arrange
+            var problem = new Problem13(new Problem13Input());
+
+            // Act
+            var result = problem.Answer;
+
+            // Assert
+            Assert.AreEqual("5537376230", result);
+        }
+    }
+}
