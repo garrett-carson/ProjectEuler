8c09157f26992096bbfad262bf7957bde7af1e64
Garrett Carson <Garrett.Carson@bdcsoft.com>
2015-09-20 15:18:47 -0500

    Problem 14

---
+++ ProjectEuler.Lib/Problem14.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Numerics;
+using ProjectEuler.Lib.Helpers;
+
+namespace ProjectEuler.Lib {
+
+    // The following iterative sequence is defined for the set of positive integers:
+    // 
+    // n → n/2 (n is even)
+    // n → 3n + 1 (n is odd)
+    // 
+    // Using the rule above and starting with 13, we generate the following sequence:
+    // 
+    // 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+    // It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+    // 
+    // Which starting number, under one million, produces the longest chain?
+    // 
+    // NOTE: Once the chain starts the terms are allowed to go above one million.
+    [Export(typeof(Problem))]
+    public class Problem14 : Problem {
+
+        public override string Run() {
+            return LongestCollatzSequenceInRange(1000000).ToString();
+        }
+
+        private int LongestCollatzSequenceInRange(int max) {
+            return Enumerable.Range(1, max - 1).OrderByDescending(x => CollatzSequence(x).Count()).First();
+        }
+
+        public IEnumerable<ulong> CollatzSequence(int start) {
+            ulong next = (ulong)start;
+            yield return next;
+            while (next > 1) {
+                next = nextColatz(next);
+                yield return next;
+            }
+        }
+
+        private ulong nextColatz(ulong next) {
+            if (next % 2 == 0) {
+                return next / 2;
+            } else {
+                return 3 * next + 1;
+            }
+        }
+    }
+}
---
+++ ProjectEuler.Test/Problem14Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler.Lib;
+
+namespace ProjectEuler.Test {
+    [TestClass]
+    public class Problem14Test {
+        [TestMethod]
+        public void Problem14Example() {
+            // Arrange
+            var problem = new Problem14();
+
+            // Act
+            var result = problem.CollatzSequence(13).ToArray();
+
+            // Assert
+            CollectionAssert.AreEqual(new ulong[] { 13, 40, 20, 10, 5, 16, 8, 4, 2, 1 }, result);
+        }
+
+        [TestMethod]
+        public void Problem14Answer() {
+            // Arrange
+            var problem = new Problem14();
+
+            // Act
+            var result = problem.Answer;
+
+            // Assert
+            Assert.AreEqual("837799", result);
+        }
+    }
+}
